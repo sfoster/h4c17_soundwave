d1536685fbcc81072426581825393899fd479647
Ted Brown <ted@oreganik.com>
2017-04-09 01:17:36 -0700

    MicClap and MicPitch: Stub Files

---
+++ Assets/Scripts/AudioAnalysis/MicClap.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicClap : MonoBehaviour {
+
+	public MicMonitor micMonitor;
+
+	public delegate void MicClapEvent ();
+	public MicClapEvent OnMicClapEvent;
+
+	private float lowerBounds;
+	private float upperBounds;
+
+	public void SetActive (bool isActive)
+	{
+		if (isActive)
+		{
+			micMonitor.processNewMicrophoneRMS += HandleNewMicrophoneLoudness;
+		}
+		else
+		{
+			micMonitor.processNewMicrophoneRMS -= HandleNewMicrophoneLoudness;
+		}
+	}
+
+	public void SetLowerBounds (float lowerBounds)
+	{
+	}
+
+	public void SetUpperBounds (float upperBounds)
+	{
+	}
+
+	private void HandleNewMicrophoneLoudness (float loudness)
+	{
+		bool detectedClap = false;
+
+		// process
+
+		// Broadcast to delegates
+		if (detectedClap && OnMicClapEvent != null)
+		{
+			OnMicClapEvent();
+		}
+	}
+}
---
+++ Assets/Scripts/AudioAnalysis/MicPitch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicPitch : MonoBehaviour {
+
+	public MicMonitor micMonitor;
+
+	private float normalizedPitch;
+	private float lowerBounds;
+	private float upperBounds;
+
+	// returns a value of zero to one
+	public float GetNormalizedPitch ()
+	{
+		return normalizedPitch;
+	}
+
+	public void SetActive (bool isActive)
+	{
+		if (isActive)
+		{
+			micMonitor.processNewMicrophoneFFT += HandleNewMicrophoneFFT;
+		}
+		else
+		{
+			micMonitor.processNewMicrophoneFFT -= HandleNewMicrophoneFFT;
+		}
+	}
+
+	public void SetLowerBounds (float lowerBounds)
+	{
+	}
+
+	public void SetUpperBounds (float upperBounds)
+	{
+	}
+
+	private void HandleNewMicrophoneFFT (float[] buffer)
+	{
+		// determine what the loudest frequency is
+		float loudestFrequency = 0;
+
+		normalizedPitch = Mathf.Clamp01((upperBounds - loudestFrequency) / (upperBounds - lowerBounds));
+	}
+}
