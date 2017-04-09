eedd78d4b2e2302ba2b181360d1e366e9cbb0bd8
Sam Foster <sfoster@mozilla.com>
2017-04-08 18:39:54 -0700

    Add normalization of the dominant frequency with calibration via up/down keys

--- Assets/Scripts/AudioAnalysis/MicVisualizer.cs
+++ Assets/Scripts/AudioAnalysis/MicVisualizer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,8 +13,18 @@ public class MicVisualizer : MonoBehaviour {
     Vector3 deltaPosition;
     Vector3 endPosition;
 
+	int MAX_FREQUENCY = 2147483647;
+	int upperFrequency;
+	int lowerFrequency;
+
+	bool captureHighFrequency = false;
+	bool captureLowFrequency = false;
+
 	void Start () 
 	{
+		upperFrequency = 0; 
+		lowerFrequency = MAX_FREQUENCY;
+
 		lineRenderer = GetComponent<LineRenderer>();
 		MeshRenderer r = GetComponent<MeshRenderer>();
         Bounds b = r.bounds;
@@ -31,25 +42,73 @@ public class MicVisualizer : MonoBehaviour {
 		MicMonitor.Instance.processNewMicrophoneFFT += PlotFrequency;
 	}
 
-	void PlotFrequency (float[] buffer) {
-		float highest = buffer[0];
-		int highIdx = 0;
+	void Update ()
+	{
+		// reset calibration on arrow key down
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			upperFrequency = 0;
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			lowerFrequency = MAX_FREQUENCY;
+		}
+		// update frequency calibration on next mic input
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			Debug.Log ("Up key");
+			captureHighFrequency = true;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			Debug.Log ("Down key");
+			captureLowFrequency = true;
+		}
+	}
+
+	int getDominantFrequencyIndex(float[] buffer) 
+	{
+		float max = buffer[0];
+		int index = 0;
 		for (int i = 0; i < buffer.Length; i++) {
-			if (buffer [i] > highest) {
-				highest = buffer [i];
-				highIdx = i;
+			if (buffer [i] > max) {
+				max = buffer [i];
+				index = i;
 			}
 		}
-		float pcent = (float) highIdx / (float) buffer.Length;
+		return index;
+	}
+
+	void PlotFrequency (float[] buffer) {
+		if (captureHighFrequency) {
+			int upperIndex = getDominantFrequencyIndex (buffer);
+			if (upperIndex > upperFrequency) {
+				upperFrequency = upperIndex;
+				Debug.Log ("new upperFrequency: " + upperFrequency);
+			}
+			captureHighFrequency = false;
+		}
+		if (captureLowFrequency) {
+			int lowerIndex = getDominantFrequencyIndex (buffer);
+			if (lowerIndex < lowerFrequency) {
+				lowerFrequency = lowerIndex;
+				Debug.Log ("new lowerFrequency: " + lowerFrequency);
+			}
+			captureLowFrequency = false;
+		}
+
+		int freqIndex = getDominantFrequencyIndex(buffer);
+		int freqRange = Math.Min(buffer.Length, upperFrequency - lowerFrequency);
+		freqIndex = Math.Max(lowerFrequency, Math.Min(freqIndex, upperFrequency));
+		// the frequency as a float within the calibrated upper/lower range
+		float pcent = (float)(freqIndex - lowerFrequency) / freqRange;
+		Debug.Log ("pcent: " + pcent);
 		lineRenderer.positionCount = 2;
 		lineRenderer.SetPosition (0, beginPosition);
 		lineRenderer.SetPosition (1, beginPosition + deltaPosition*pcent);
-		Debug.Log(pcent);
+
+		// Debug.Log(pcent);
 	}
 
 	void PlotAllFrequencies (float[] buffer)
 	{
-		Debug.Log(buffer.Length);
+		// Debug.Log(buffer.Length);
 		lineRenderer.positionCount = buffer.Length;
 		float smoothing = 5.0f;
 
