11ec1d0df220fa6a12220f2692de58a469b2d0af
Ted Brown <ted@oreganik.com>
2017-04-09 01:28:19 -0700

    Calibrate mic stubs

---
+++ Assets/Scripts/AudioAnalysis/CalibrateMicPitch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalibrateMicPitch : MonoBehaviour {
+
+	public MicPitch micPitch;
+
+	private float upperFrequencyIndex;
+	private float lowerFrequencyIndex;
+
+	public void SetActive (bool isActive)
+	{
+		if (isActive)
+		{
+			micPitch.micMonitor.processNewMicrophoneFFT += ProcessBuffer;
+		}
+		else
+		{
+			micPitch.micMonitor.processNewMicrophoneFFT -= ProcessBuffer;
+		}
+	}
+
+	void ProcessBuffer (float[] buffer)
+	{
+		float max = buffer[0];
+		int dominantFrequencyIndex = 0;
+		for (int i = 0; i < buffer.Length; i++) 
+		{
+			if (buffer [i] > max) 
+			{
+				max = buffer [i];
+				dominantFrequencyIndex = i;
+			}
+		}
+
+		if (dominantFrequencyIndex < lowerFrequencyIndex) 
+		{
+			lowerFrequencyIndex = dominantFrequencyIndex;
+			micPitch.SetLowerBounds(lowerFrequencyIndex);
+		}
+
+		if (dominantFrequencyIndex > upperFrequencyIndex) 
+		{
+			upperFrequencyIndex = dominantFrequencyIndex;
+			micPitch.SetUpperBounds(upperFrequencyIndex);
+		}
+	}
+
+}
